7f3598732faf969804b5ec2704757b1626870ebb
1106883 <1106883@rgu.ac.uk>
2016-03-21 10:47:35 +0000

    hi

---
+++ Lab6/insertSuperhero.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1106883
+ * Date: 21/03/2016
+ * Time: 10:38
+ */
+$db= new mysqli(
+    "eu-cdbr-azure-west-d.cloudapp.net",
+    "b272f5632b6bab",
+    "fda91417",
+    "Superhero"
+);
+
+if($db -> connect_errno){
+    die('Connect Failed['.$db->connect_error.']');
+}
+
+$firstname = $_POST['firstname'];
+$surname = $_POST['surname'];
+$power = $_POST['power'];
+
+
+$sql = "INSERT INTO 'Superhero' VALUES ".$firstname.", ".$surname.", ".$power;
+
+
+?>
---
+++ Lab6/superheroForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1106883
+ * Date: 21/03/2016
+ * Time: 10:09
+ */
+echo "<form action='insertSuperhero.php' method='post'>
+    <table>
+        <tr><td><label>Forename </label><input type ='text' name='forename'></td></tr>
+        <tr><td><label>Surname </label><input type ='text' name='surname'></td></tr>
+        <tr><td><label>Superpower </label><input type ='text' name='power'></td></tr>
+        <tr><td><input type='submit' name='Submit'></td></tr>
+    </table>";
+?>
