ac6fd885884d4e8fa0be38571f4e8a83783985b8
1106883 <1106883@rgu.ac.uk>
2016-03-21 10:01:29 +0000

    hi

---
+++ Lab6/HelloPrinter.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1106883
+ * Date: 21/03/2016
+ * Time: 09:42
+ */
+echo "Hello".$planet;
+?>
---
+++ Lab6/HelloSelector.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1106883
+ * Date: 21/03/2016
+ * Time: 09:41
+ */
+echo "Where would you like to say hello?";
+echo "<a href=HelloPrinter.php?planet=earth>Earth</a>";
+echo "<a href=HelloPrinter.php?planet=mars>Mars</a>";
+echo "<a href=HelloPrinter.php?planet=uranus>Uranus</a>";
+?>
